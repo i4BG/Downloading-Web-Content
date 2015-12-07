b8289626ee583f68288788d1b0febf5eb5fdb425
Ivaylo Todorov <ivaylo.k.todorov@me.com>
2015-12-07 22:02:02 +0200

    DownloadWebContent-iOS
    how to download web content on iOS

--- Downloading Web Content/ViewController.swift
+++ Downloading Web Content/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        
+   let url = NSURL(string: "http://abv.bg")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
 
+            if let urlContent = data {
+                
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                
+                self.webView.loadHTMLString(String(webContent!), baseURL: nil)
+                    
+                })
+                
+                
+            } else {
+                
+                print("error")
+                
+            }
+            
+        }
+            
+        
+        task.resume()
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +52,3 @@ class ViewController: UIViewController {
 
 
 }
-
